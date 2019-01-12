26ba872a72f417ba10c05b7e0978df1cb8cf7916
Qi Wang <qi@broadinstitute.org>
2019-01-12 12:38:01 -0500

    hook up in kubernetes

--- core/src/main/scala/org/broadinstitute/workbench/ccm/db/SqlConnection.scala
+++ core/src/main/scala/org/broadinstitute/workbench/ccm/db/SqlConnection.scala
@@ -1,12 +1,15 @@
 package org.broadinstitute.workbench.ccm.db
 
-import cats.effect.{Async, ContextShift}
+import cats.effect.{Async, ContextShift, IO}
+import ciris.{envF, loadConfig}
 import doobie.util.transactor.Transactor
+import ciris.cats.effect._
+import cats.implicits._
 
 class SqlConnection[F[_]: ContextShift: Async](config: SqlConfig) {
   val dbTransactor: Transactor[F] = Transactor.fromDriverManager[F](
     "org.postgresql.Driver", // driver classname
-    config.url.asString, // connect URL (driver-specific)
+    s"jdbc:postgresql://127.0.0.1:5432/ccm", // connect URL (driver-specific)
     config.user.asString,              // user
     config.password.asString                       // password
   )
@@ -14,9 +17,15 @@ class SqlConnection[F[_]: ContextShift: Async](config: SqlConfig) {
 
 object SqlConnection {
   def apply[F[_]: ContextShift: Async](config: SqlConfig): SqlConnection[F] = new SqlConnection[F](config)
+
+  val readCondigFromEnv: IO[SqlConfig] = loadConfig(
+    envF[IO, String]("DB_USER"),
+    envF[IO, String]("DB_PASSWORD")
+  ){ (username, password) =>
+    SqlConfig(DbUser(username), DbPassword(password))
+  }.flatMap(x => IO.fromEither(x.leftMap(_.toException).leftWiden))
 }
 
-final case class DbUrl(asString: String) extends AnyVal
 final case class DbUser(asString: String) extends AnyVal
 final case class DbPassword(asString: String) extends AnyVal
-final case class SqlConfig(url: DbUrl, user: DbUser, password: DbPassword)
+final case class SqlConfig(user: DbUser, password: DbPassword)
--- core/src/test/scala/org/broadinstitute/workbench/ccm/db/DBConfig.scala
+++ core/src/test/scala/org/broadinstitute/workbench/ccm/db/DBConfig.scala
@@ -3,7 +3,6 @@ package db
 
 object DBConfig {
   val dbConfig = SqlConfig(
-    DbUrl("jdbc:postgresql://127.0.0.1:5432/ccm"),
     DbUser("ccm"),
     DbPassword("123456")
   )
