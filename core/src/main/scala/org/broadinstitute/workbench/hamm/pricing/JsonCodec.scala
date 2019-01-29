664eaaa651a3cf1c82acdf5e1c5cabd77e542713
ansingh7115 <singh.an@husky.neu.edu>
2019-01-29 14:46:23 -0500

    decoder tweak

--- core/src/main/scala/org/broadinstitute/workbench/hamm/pricing/JsonCodec.scala
+++ core/src/main/scala/org/broadinstitute/workbench/hamm/pricing/JsonCodec.scala
@@ -8,11 +8,11 @@ object JsonCodec {
 
   implicit val categoryDecoder: Decoder[Category] = Decoder.instance { cursor =>
     for {
+      resourceFamily <- cursor.downField("resourceFamily").as[ResourceFamily]
       serviceDisplayName <- cursor.downField("serviceDisplayName").as[String]
-      resourceFamily <- cursor.downField("resourceFamily").as[String]
       resourceGroup <- cursor.downField("resourceGroup").as[String]
       usageType <- cursor.downField("usageType").as[String]
-    } yield Category(ServiceDisplayName(serviceDisplayName), ResourceFamily.stringToResourceFamily(resourceFamily), ResourceGroup(resourceGroup), UsageType.stringToUsageType(usageType))
+    } yield Category(ServiceDisplayName(serviceDisplayName), resourceFamily, ResourceGroup(resourceGroup), UsageType.stringToUsageType(usageType))
   }
 
   implicit val tieredRateDecoder: Decoder[TieredRate] = Decoder.instance { cursor =>
@@ -34,24 +34,25 @@ object JsonCodec {
   implicit val googlePriceItemDecoder: Decoder[GooglePriceItem] = Decoder.instance {
     cursor =>
       for {
-        resourceFamily <- cursor.downField("category").downField("resourceFamily").as[String]
-        _ <- if (ResourceFamily.stringToResourceFamily.keys.toList.contains(resourceFamily)) Right(resourceFamily) else Left(DecodingFailure(s"Irrelavent item with resource family $resourceFamily", List()))
+        category <- cursor.downField("category").as[Category] // we're evaluating on category first because we want to filter by resource family
         name <- cursor.downField("name").as[String]
         skuId <- cursor.downField("skuId").as[String]
         description <- cursor.downField("description").as[String]
-        category <- cursor.downField("category").as[Category]
         regions <- cursor.downField("serviceRegions").as[List[String]]
         pricingInfo <- cursor.downField("pricingInfo").as[List[PricingInfo]]
       } yield GooglePriceItem(SkuName(name), SkuId(skuId), SkuDescription(description), category, regions.map(Region.stringToRegion(_)), pricingInfo)
   }
 
+  implicit val resourceFamily: Decoder[ResourceFamily] = Decoder.decodeString.emap{
+    resourceFamily => ResourceFamily.stringToResourceFamily.get(resourceFamily).toRight(s"Irrelavent item with resource family $resourceFamily")
+  }
+
   implicit val listGooglePriceItemDecoder: Decoder[List[GooglePriceItem]] = Decoder.decodeList(Decoder[GooglePriceItem].either(Decoder[Json])).map {
     x =>
-      val thing = x.flatMap{
+      x.flatMap{
         item =>
           item.left.toOption
       }
-      thing
   }
 
   implicit val googlePriceListDecoder: Decoder[GooglePriceList] = Decoder.instance { cursor =>
