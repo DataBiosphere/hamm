dea814da106b81aaa8d93b8bed80d0e5f2038d05
ansingh7115 <singh.an@husky.neu.edu>
2019-02-05 21:50:46 -0500

    refactor core

--- automation/src/test/scala/org/broadinstitute/workbench/hamm/automation/WorkflowCostTest.scala
+++ automation/src/test/scala/org/broadinstitute/workbench/hamm/automation/WorkflowCostTest.scala
@@ -1,48 +1,50 @@
-//package org.broadinstitute.workbench.hamm
-//package automation
-//
-//import cats.effect.IO
-//import fs2.Stream
-//import io.grpc.Metadata
-//import minitest.laws.Checkers
-//import org.broadinstitute.workbench.hamm.protos.hamm._
-//import org.lyranthe.fs2_grpc.java_runtime.implicits._
-//import server.Generators._
-//
-//object WorkflowCostTest extends HammTestSuite with Checkers {
-//  val defaultMetaData = new Metadata()
-////  val authKey = Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER)
-////  import sys.process._
-////  val token: String = "gcloud auth print-access-token".!!
-////  defaultMetaData.put(authKey, s"Bearer $token")
-//
-////  TODO: put this in a different file
-//  test("status should return build info"){
-//    val res = for {
-//      managedChannel <- Stream.resource(TestDependencies.managedChannelResource)
-//      ccmStub = HammFs2Grpc.stub[IO](managedChannel)
-//      response <- Stream.eval(ccmStub.status(StatusRequest(), defaultMetaData))
-//    } yield {
-//      assert(response.sbtVersion.contains("1.2.8"))
-//    }
-//
-//    res.compile.last.unsafeRunSync().get
-//  }
-//
-//  test("workflow getCost should calculate compute cost"){
-//    check1{
-//      (workflowCostRequest: WorkflowCostRequest) =>
-//        val res = for {
-//          managedChannel <- Stream.resource(TestDependencies.managedChannelResource)
-//          workflowStub = HammFs2Grpc.stub[IO](managedChannel)
-//          response <- Stream.eval(workflowStub.getWorkflowCost(workflowCostRequest, defaultMetaData))
-//        } yield {
-//          val expectedResponse = WorkflowCostResponse(0.06899999999999999)
-//          assert(response == expectedResponse)
-//        }
-//
-//        res.compile.last.unsafeRunSync().get
-//        true
-//    }
-//  }
-//}
+package org.broadinstitute.workbench.hamm
+package automation
+
+import cats.effect.IO
+import fs2.Stream
+import io.grpc.Metadata
+import minitest.laws.Checkers
+import org.broadinstitute.workbench.hamm.protos.hamm._
+import server.Generators._
+
+object WorkflowCostTest extends HammTestSuite with Checkers {
+  val defaultMetaData = new Metadata()
+//  val authKey = Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER)
+//  import sys.process._
+//  val token: String = "gcloud auth print-access-token".!!
+//  defaultMetaData.put(authKey, s"Bearer $token")
+
+//  TODO: put this in a different file
+  test("status should return build info"){
+    val res = for {
+      managedChannel <- Stream.resource(TestDependencies.managedChannelResource)
+      ccmStub = HammFs2Grpc.stub[IO](managedChannel)
+      response <- Stream.eval(ccmStub.status(StatusRequest(), defaultMetaData))
+    } yield {
+      println(response.toString)
+      assert(response.sbtVersion.contains("1.2.8"))
+    }
+    println("DEFAULTMETADATA: " + defaultMetaData)
+    println("managedChannelResource: " + TestDependencies.managedChannelResource.toString)
+
+    res.compile.last.unsafeRunSync().get
+  }
+
+  test("workflow getCost should calculate compute cost"){
+    check1{
+      (workflowCostRequest: WorkflowCostRequest) =>
+        val res = for {
+          managedChannel <- Stream.resource(TestDependencies.managedChannelResource)
+          workflowStub = HammFs2Grpc.stub[IO](managedChannel)
+          response <- Stream.eval(workflowStub.getWorkflowCost(workflowCostRequest, defaultMetaData))
+        } yield {
+          val expectedResponse = WorkflowCostResponse(0.06899999999999999)
+          assert(response == expectedResponse)
+        }
+
+        res.compile.last.unsafeRunSync().get
+        true
+    }
+  }
+}
